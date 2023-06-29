ecc19d6eb59135eae0fe0d3deb6d8d97f1066169
daehan.lawplatform <daehan@lawplatform.kr>
2023-06-29 17:05:05 +0900

    feat: add spotLight

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
-import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Environment, OrbitControls, PerspectiveCamera, PointerLockControls } from "@react-three/drei";
 import Box from "./model/Box"
 import { Suspense } from "react";
+import Ocean from "./world/Ocean";
 function App() {
 	return (
 		<Suspense fallback={null}>
-
-			<Canvas camera={{ position: [0, 0, 2] }}>
+			<Canvas>
 				<Environment
 					files="/sky.hdr"
 					background
 				/>
-				<OrbitControls target={[0, 0.35, 0]} maxPolarAngle={1.45} />
-				<PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+				<PerspectiveCamera fov={50} />
+				<OrbitControls target={[100, 0.35, 0]} maxPolarAngle={1.45} enableRotate={false} enablePan={true} enableZoom={false} />
+
+				<spotLight
+					color={[1, 0.25, 0, 7]}
+					intensity={1.5}
+					angle={0.6}
+					penumbra={0.5}
+					position={[5, 5, 0]}
+					castShadow
+					shadow-bias={-0.0001}
+				/>
+
+
+
 				<Box />
+				<Ocean></Ocean>
 			</Canvas >
 		</Suspense>
 	);
--- src/model/Box.tsx
+++ src/model/Box.tsx
@@ -1,6 +1,6 @@
 export default function Box() {
 	return (
-		<mesh>
+		<mesh position={[-2, 100, 100]}>
 			<boxGeometry />
 			<meshBasicMaterial color={0x00ff00} wireframe />
 		</mesh>
